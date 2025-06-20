55b1e8d41e754aa8055e28ac44f8731d6efe111c
JacksonSmith43 <jessica444@gmx.at>
2025-06-20 10:27:37 +0200

    267. Registering Multiple Routes.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,7 +1,14 @@
 import { Routes } from "@angular/router";
 import { TasksComponent } from "./tasks/tasks.component";
+import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 
-export const routes: Routes = [{
-    path: "tasks", // This is is the path that will be used in the URL.
-    component: TasksComponent,
-}]
+export const routes: Routes = [
+    {
+        path: "", // This is the default path that will be used when the application loads. 
+        component: NoTaskComponent
+    },
+    {
+        path: "tasks", // This is is the path that will be used in the URL.
+        component: TasksComponent,
+    }
+]
