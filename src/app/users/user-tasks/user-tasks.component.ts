05789e5134f459e0e16f1a2d855930264f4c77a4
JacksonSmith43 <jessica444@gmx.at>
2025-06-20 12:24:05 +0200

    273. Extracting Dynamic Route Parameters via Observables.

--- src/app/users/user-tasks/user-tasks.component.ts
+++ src/app/users/user-tasks/user-tasks.component.ts
@@ -1,5 +1,6 @@
-import { Component, input, inject, computed } from '@angular/core';
+import { Component, input, inject, computed, OnInit, DestroyRef } from '@angular/core';
 import { UsersService } from '../users.service';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-user-tasks',
@@ -7,9 +8,19 @@ import { UsersService } from '../users.service';
   templateUrl: './user-tasks.component.html',
   styleUrl: './user-tasks.component.css',
 })
-export class UserTasksComponent {
-  userId = input.required<string>();
+export class UserTasksComponent implements OnInit {
+  userName = "";
   private usersService = inject(UsersService);
+  private activeRoute = inject(ActivatedRoute); // ActivatedRoute is used to access the route parameters. 
+  private destroyRef = inject(DestroyRef);
 
-  userName = computed(() => this.usersService.users.find(u => u.id === this.userId())?.name);
+  ngOnInit() {
+    console.log(this.activeRoute);
+    const subscription = this.activeRoute.paramMap.subscribe({ // This is a subscription to the paramMap observable of the ActivatedRoute which will emit whenever the route parameters change.
+      next: paramMap => { // This will be called whenever the route parameters change meaning when the userId changes. 
+        this.userName = this.usersService.users.find((u) => u.id === paramMap.get("userId"))?.name || ""; // We are using the userId from the route parameters to find the user in the users array and get the name of the user. If the user is not found, we set the userName to an empty string.
+      }
+    })
+    this.destroyRef.onDestroy(() => subscription.unsubscribe())
+  }
 }
