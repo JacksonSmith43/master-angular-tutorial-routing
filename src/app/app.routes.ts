96473776db442e0921912f58f88635c2dde6d85d
JacksonSmith43 <jessica444@gmx.at>
2025-06-23 09:32:05 +0200

    281. Splitting Route Definitions Across Multiple Files.

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,8 @@ import { Routes } from "@angular/router";
 
 import { NoTaskComponent } from "./tasks/no-task/no-task.component";
 import { UserTasksComponent } from "./users/user-tasks/user-tasks.component";
-import { TasksComponent } from "./tasks/tasks.component";
-import { NewTaskComponent } from "./tasks/new-task/new-task.component";
 import { NotFoundComponent } from "./not-found/not-found.component";
+import { routes as userRoutes } from "./users/users.routes";
 
 export const routes: Routes = [
     {
@@ -14,24 +13,10 @@ export const routes: Routes = [
     {
         path: "users/:userId", // URL: path: <domain>/users/<u1>
         component: UserTasksComponent,
-        children: [
-            {
-                path: "",
-                redirectTo: "tasks",
-                pathMatch: "prefix"
-            },
-            {
-                path: "tasks", // This path will be appended to the parent path, resulting in <domain>/users/<u1>/tasks
-                component: TasksComponent
-            },
-            {
-                path: "tasks/new",
-                component: NewTaskComponent
-            }
-        ]
+        children: userRoutes, // This will load the user-specific routes defined in users.routes.ts. 
     },
     {
         path: "**", // This is a wildcard path that will match any path that is not defined above. It is used to show a 404 page. 
         component: NotFoundComponent
     }
-]
+];
---
+++ src/app/users/users.routes.ts
@@ -0,0 +1,19 @@
+import { Routes } from "@angular/router";
+import { TasksComponent } from "../tasks/tasks.component";
+import { NewTaskComponent } from "../tasks/new-task/new-task.component";
+
+export const routes: Routes = [
+    {
+        path: "", // This is the default path for the user tasks route, which will be used when the user navigates to <domain>/users/<u1>
+        redirectTo: "tasks",
+        pathMatch: "prefix"
+    },
+    {
+        path: "tasks", // This path will be appended to the parent path, resulting in <domain>/users/<u1>/tasks
+        component: TasksComponent
+    },
+    {
+        path: "tasks/new",
+        component: NewTaskComponent
+    }
+]
