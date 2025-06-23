203dc16695b83721241611b51718221753198177
JacksonSmith43 <jessica444@gmx.at>
2025-06-23 10:31:16 +0200

    286. Using Query Parameters For Data Manipulation.

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,4 +1,4 @@
-import { Component, input, inject, computed, OnInit, DestroyRef } from '@angular/core';
+import { Component, input, inject, computed, OnInit, DestroyRef, signal } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { TasksService } from './tasks.service';
@@ -15,16 +15,26 @@ import { ActivatedRoute, RouterLink } from '@angular/router';
 export class TasksComponent implements OnInit {
   private tasksService = inject(TasksService);
   userId = input.required<string>();
-  order?: 'asc' | 'desc';
+  order = signal<'asc' | 'desc'>('desc');
 
-  userTasks = computed(() => this.tasksService.allTasks().filter((task) => task.userId === this.userId())); // Filters tasks by userId. computed is used to automatically update when userId changes. 
+  userTasks = computed(() => this.tasksService.allTasks()
+    .filter((task) => task.userId === this.userId()) // Filters tasks by userId. computed is used to automatically update when userId changes. 
+    .sort((a, b) => { // Sorts tasks by id in ascending or descending order based on the order signal.
+      if (this.order() === 'desc') {
+        return a.id > b.id ? -1 : 1; // If order is 'desc', sort in descending order. -1 means a comes before b, 1 means b comes before a.
+
+      } else {
+        return a.id > b.id ? 1 : -1;
+      }
+    })
+  );
 
   private activatedRoute = inject(ActivatedRoute);
   private destroyRef = inject(DestroyRef);
 
   ngOnInit(): void {
     const subscription = this.activatedRoute.queryParams.subscribe({ // This is a subscription to the queryParams observable of the ActivatedRoute which will emit whenever the query parameters change.
-      next: params => (this.order = params['order']), // params['order'] will contain the current order query parameter value which will be saved in the order input property. 
+      next: params => (this.order.set(params['order'])), // params['order'] will contain the current order query parameter value which will be saved in the order input property. 
     });
     this.destroyRef.onDestroy(() => subscription.unsubscribe());
   }
