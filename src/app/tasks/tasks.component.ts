e9ecd0df840ac614eee703ac072ac236a8716300
JacksonSmith43 <jessica444@gmx.at>
2025-06-23 10:17:15 +0200

    285. Extracting Query Parameters via Observables.

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,8 +1,8 @@
-import { Component, input, inject, computed } from '@angular/core';
+import { Component, input, inject, computed, OnInit, DestroyRef } from '@angular/core';
 
 import { TaskComponent } from './task/task.component';
 import { TasksService } from './tasks.service';
-import { RouterLink } from '@angular/router';
+import { ActivatedRoute, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-tasks',
@@ -12,10 +12,20 @@ import { RouterLink } from '@angular/router';
   imports: [TaskComponent, RouterLink],
 })
 
-export class TasksComponent {
+export class TasksComponent implements OnInit {
   private tasksService = inject(TasksService);
   userId = input.required<string>();
-  order = input<'asc' | 'desc'>();
+  order?: 'asc' | 'desc';
 
   userTasks = computed(() => this.tasksService.allTasks().filter((task) => task.userId === this.userId())); // Filters tasks by userId. computed is used to automatically update when userId changes. 
+
+  private activatedRoute = inject(ActivatedRoute);
+  private destroyRef = inject(DestroyRef);
+
+  ngOnInit(): void {
+    const subscription = this.activatedRoute.queryParams.subscribe({ // This is a subscription to the queryParams observable of the ActivatedRoute which will emit whenever the query parameters change.
+      next: params => (this.order = params['order']), // params['order'] will contain the current order query parameter value which will be saved in the order input property. 
+    });
+    this.destroyRef.onDestroy(() => subscription.unsubscribe());
+  }
 }
