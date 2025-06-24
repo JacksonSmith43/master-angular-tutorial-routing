0e3bfddbaac1b0f38c9b74f10f6c5697b77caf33
JacksonSmith43 <jessica444@gmx.at>
2025-06-24 09:26:06 +0200

    302. Lazy Loading Entire Route Groups.

--- src/app/users/users.routes.ts
+++ src/app/users/users.routes.ts
@@ -1,31 +1,7 @@
-import { ResolveFn, Routes } from '@angular/router';
-import { inject } from '@angular/core';
+import { Routes } from '@angular/router';
 
 import { canLeaveEditGuard, NewTaskComponent } from '../tasks/new-task/new-task.component';
-import { Task } from '../tasks/task/task.model';
-import { TasksService } from '../tasks/tasks.service';
-
-
-const resolveUserTasks: ResolveFn<Task[]> = (activatedRouteSnapshot, routerState) => {
-
-    const order = activatedRouteSnapshot.queryParams['order'];
-    const tasksService = inject(TasksService);
-
-    const tasks = tasksService
-        .allTasks()
-        .filter(
-            (task) => task.userId === activatedRouteSnapshot.paramMap.get('userId')
-        );
-
-    if (order && order === 'asc') {
-        tasks.sort((a, b) => (a.id > b.id ? 1 : -1));
-
-    } else {
-        tasks.sort((a, b) => (a.id > b.id ? -1 : 1));
-    }
-
-    return tasks.length ? tasks : [];
-};
+import { resolveUserTasks, TasksComponent } from '../tasks/tasks.component';
 
 
 export const routes: Routes = [
@@ -36,7 +12,7 @@ export const routes: Routes = [
     },
     {
         path: 'tasks', // <your-domain>/users/<uid>/tasks
-        loadComponent: () => import("../tasks/tasks.component").then(mod => mod.TasksComponent),
+        component: TasksComponent,
         runGuardsAndResolvers: "always", // This ensures that the component is reloaded when the query parameters change. Otherwise, the component would not be reloaded when the query parameters change and only when the route parameters change. Route parameters are the parameters defined in the path, such as userId in users/:userId. Query parameters are the parameters defined in the URL after the question mark, such as ?order=asc. 
         resolve: {
             userTasks: resolveUserTasks,
